13e55b38734152f24f4e510d748338dde63aa421
a-j-sheilla <sheillajuma001@gmail.com>
2024-11-13 21:47:17 +0300

    main entry point of the portfolio

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tmpl = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/projects", projectsHandler)
+	http.HandleFunc("/contact", contactHandler)
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Server failed:", err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.html")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.html")
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "projects.html")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "contact.html")
+}
+
+func renderTemplate(w http.ResponseWriter, tmplName string) {
+	if err := tmpl.ExecuteTemplate(w, tmplName, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
